9cb7052fc6a5ffb9c17cee1f967ff3d0d0a3d374
Van Hung <vanhungnguyenbku@gmail.com>
2021-11-04 00:37:03 +0700

    setup

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Sidebar from '../sidebar/Sidebar'
+import Routes from '../Routes'
+
+const Layout = () => {
+	return <div>Hello Layout</div>
+}
+
+export default Layout
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/grid.css'
+import './assets/css/index.css'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import Layout from './components/layout/Layout'
+
+document.title = 'Tua CRM'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<Layout />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
