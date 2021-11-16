8c0cc72dbc844189c207dcd3595ec6e754a0c351
VanHungNguyen <vanhungnguyenbku@gmail.com>
2021-11-16 18:02:54 +0700

    customer table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './table.css'
 
 const Table = (props) => {
+	const initDataShow =
+		props.limit && props.bodyData
+			? props.bodyData.slice(0, Number(props.limit))
+			: props.bodyData
+
+	const [dataShow, setDataShow] = useState(initDataShow)
+
+	let pages = 1
+
+	let range = []
+
+	if (props.limit !== undefined) {
+		let page = Math.floor(props.bodyData.length / Number(props.limit))
+		pages =
+			props.bodyData.length % Number(props.limit) === 0 ? page : page + 1
+
+		range = [...Array(pages).keys()]
+	}
+
 	return (
 		<div>
 			<div className='table-wrapper'>
@@ -17,13 +36,24 @@ const Table = (props) => {
 					) : null}
 					{props.bodyData && props.renderBody ? (
 						<tbody>
-							{props.bodyData.map((item, i) => {
+							{dataShow.map((item, i) => {
 								return props.renderBody(item, i)
 							})}
 						</tbody>
 					) : null}
 				</table>
 			</div>
+			{pages > 1 ? (
+				<div className='table__pagination'>
+					{range.map((item, i) => {
+						return (
+							<div key={i} className={`table__pagination-item`}>
+								{item + 1}
+							</div>
+						)
+					})}
+				</div>
+			) : null}
 		</div>
 	)
 }
